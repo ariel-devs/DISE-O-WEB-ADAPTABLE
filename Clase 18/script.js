988bbc8d280d0534443ed68194796905fd063258
Ariel <96531908+ariel-devs@users.noreply.github.com>
2026-09-22 11:38:19 -0600

    Update

---
+++ Clase 18/script.js
@@ -0,0 +1,38 @@
+console.log("Script Cargado Correctamente")
+
+//getElementById
+const titulo = document.getElementById("titulo-principal");
+const form = document.getElementById("form-contacto");
+
+//querySelector = cualquie selector CSS valido
+const boton = document.querySelector(".btn-primario");
+const nav = document.querySelector("nav");
+const primero = document.querySelector("li:first-child");
+const inputEmail = document.querySelector('input[type="email"]');
+
+//querySelectorAll
+const tarjetas = document.querySelectorAll(".tarjeta");
+const link = document.querySelectorAll("a[href]");
+
+tarjetas.forEach((tarjeta) => {
+  console.log(tarjeta.textContent);
+});
+
+const el = document.querySelector('#popup');
+if (el) {
+  console.log('popup encontrado')
+} else {
+  console.warn('popup no encontrado')
+}
+
+const btn = document.querySelector("#mi-boton");
+btn.addEventListener('click', (event) => {
+  console.log('click en: ', event.target);
+  event.target.textContent = "Click"
+})
+
+//addEventListener para formularios
+const campo = document.querySelector("#nombre");
+campo.addEventListener("input", (event) => {
+  console.log("valor: ", event.target.value);
+})
